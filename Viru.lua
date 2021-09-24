6eeb1a9c1c73b52dac3248df754256e338e61c72
Viru-Team <86714940+Viru-Team@users.noreply.github.com>
2021-09-24 03:52:16 +0300

    Update Viru.lua

--- Viru.lua
+++ Viru.lua
@@ -4349,7 +4349,7 @@ Welcome To Source
 keyboard = {} 
 keyboard.inline_keyboard = {
 {{text = '↯ Source Channel',url="https://t.me/ViruuTeam"}},
-{{text = '↯ Developer',url="VVJAA"},{text = '↯ Tws Viru',url="https://t.me/@Qusai9_bot"}},
+{{text = '↯ Developer',url="t.me/@VVJAA"},{text = '↯ Tws Viru',url="https://t.me/Qusai9_bot"}},
 }
 local msg_id = msg.id_/2097152/0.5
 https.request("https://api.telegram.org/bot"..TokenBot..'/sendPhoto?chat_id=' .. msg.chat_id_ .. '&photo=https://t.me/ViruuTeam&caption=' .. URL.escape(Text).."&reply_to_message_id="..msg_id.."&parse_mode=markdown&disable_web_page_preview=true&reply_markup="..JSON.encode(keyboard))
